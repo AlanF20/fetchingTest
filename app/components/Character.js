c272ee8758c62f6ae5ec7dd6f5cb8a9942f8a136
Alan Flores <alanif@hotmail.com>
2022-07-04 15:27:35 -0500

    refactor: adding the svg loader in the character module

--- app/components/Character.js
+++ app/components/Character.js
@@ -1,13 +1,19 @@
-import getAPIData from "../helpers/getAPIData.js"
+import getAPIData from "../helpers/getAPIData.js";
+import Loader from "./Loader.js";
 
-const Character = async () =>{
-    const $article = document.createElement("article");
-    const data = await getAPIData();
+const Character = async () => {
+  const $article = document.createElement("article");
+  const data = await getAPIData();
+  setTimeout(() => {
     $article.innerHTML = `
-        <img src="${data.image}" alt="${data.name}">
-        <h2>${data.name}</h2>
+    <img src="${data.image}" alt="${data.name}">
+    <h2>${data.name}</h2>
     `;
-    return $article;
+  }, 500);
+  $article.innerHTML = `
+        <img src="./app/assets/spinning-circles.svg">
+    `;
+  return $article;
 };
 
 export default Character;
